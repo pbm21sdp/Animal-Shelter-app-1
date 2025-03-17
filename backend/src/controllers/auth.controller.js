671eac4b66e30755a5a9a92fea949ffbd9055f78
mariusel9911 <marius.nistor@student.upt.ro>
2025-03-17 12:57:12 +0200

    Added auth enpoints in swagger

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,11 @@
-export const signup = async (req,res) => {
-    res.send("Signup route")
+export const signup = async (req, res) => {
+    res.status(201).json({ message: "User created successfully" });
 }
 
-export const login= async (req,res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    res.status(200).json({ message: "Logged in successfully" });
 }
 
-export const logout= async (req,res) => {
-    res.send("logout route")
+export const logout = async (req, res) => {
+    res.status(200).json({ message: "Logged out successfully" });
 }
