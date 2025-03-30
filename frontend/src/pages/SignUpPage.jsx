92e3528dd4f64c52eceffec72e53bd7efbe9c9d3
mariusel9911 <marius.nistor@student.upt.ro>
2025-03-30 05:06:57 +0300

    Signup and verification linked with backend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,18 +1,30 @@
 import {motion} from "framer-motion";
 import Input from "../components/Input";
-import { User, Mail, Lock } from "lucide-react";
+import {User, Mail, Lock, Loader} from "lucide-react";
 import {useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter.jsx";
+
+import {useAuthStore} from "../store/authStore.js";
 
 const SignUpPage = () => {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    const handleSignUp = (e) => {
+    const {signup, error, isLoading} = useAuthStore();
+
+    const handleSignUp = async (e) => {
         e.preventDefault();
+
+        try{
+            await signup(email, password, name);
+            navigate("/verify-email");
+        }catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -53,22 +65,25 @@ const SignUpPage = () => {
             onChange={(e) => setPassword(e.target.value)}
         />
         {/* Password strength meter*/}
+        {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
         <PasswordStrengthMeter password={password} />
-        <motion.button className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-violet-400 to-purple-600 text-white
-        font-bold rounded-lg shadow-lg hover:from-violet-600
-        hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2
-        focus:ring-offset-gray-900 transition duration-200'
 
-        whileHover={{scale: 1.02}}
-        whileTap={{scale: 0.98}}
-        type="submit"
+        <motion.button
+            className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-violet-400 to-purple-600 text-white
+						font-bold rounded-lg shadow-lg hover:from-violet-600
+						hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2
+						 focus:ring-offset-purple-800 transition duration-200'
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type='submit'
+            disabled={isLoading}
         >
-            Sign Up
+            {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
         </motion.button>
     </form>
 </div>
     <div className="px-8 py-4 bg-gray-300 bg-opacity-50 flex justify-center">
-        <p className="text-sm text-gray-400">
+        <p className="text-sm text-gray-500">
             Already have an account?{" "}
             <Link to={"/login"} className="text-violet-400 hover:underline">
                 Login
@@ -77,6 +92,8 @@ const SignUpPage = () => {
 
     </div>
     </motion.div>
+
+
     );
 };
 
