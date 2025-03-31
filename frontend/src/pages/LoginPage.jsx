e72f9fe3b48930758b024d70b5dee32173ed55fa
mariusel9911 <marius.nistor@student.upt.ro>
2025-03-31 03:32:18 +0300

    Login link with backend

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,15 +3,19 @@ import {motion} from "framer-motion";
 import {User, Loader, Lock, Mail} from "lucide-react";
 import {Link} from "react-router-dom";
 import Input from "../components/Input.jsx";
+import {useAuthStore} from "../store/authStore";
 
 const LoginPage = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const isLoading= false;
 
-    const handleLogin = (e) => {
+    const { login, isLoading, error } = useAuthStore();
+
+
+    const handleLogin = async (e) => {
         e.preventDefault();
+        await login(email, password);
     }
 
     return (
@@ -48,6 +52,7 @@ const LoginPage = () => {
                             Forgot password?
                         </Link>
                     </div>
+                    {error && <p className="text-red-500 font-semibold mb-2">{error}</p>}
 
                     <motion.button
                         whileHover={{ scale: 1.02 }}
