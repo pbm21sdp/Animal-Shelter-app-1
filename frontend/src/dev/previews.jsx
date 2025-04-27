840098a4fd89c3dd31b98151c25b24cfcbb3f31d
mariusel9911 <marius.nistor@student.upt.ro>
2025-04-28 02:15:17 +0300

    Tailwind up to date

--- frontend/src/dev/previews.jsx
+++ frontend/src/dev/previews.jsx
@@ -1,12 +1,16 @@
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import App from "../App.jsx";
+import PawsHomepage from "../pages/PawsHomePage.jsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/App">
                 <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/PawsHomepage">
+                <PawsHomepage/>
             </ComponentPreview>
         </Previews>
     )
