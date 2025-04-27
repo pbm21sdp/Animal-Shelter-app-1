1f9c72a0e28500c43e7696db7a287c3a2646889d
mariusel9911 <marius.nistor@student.upt.ro>
2025-04-28 02:17:00 +0300

    Tailwind up to date

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        tealcustom: '#114b5f',
+        dog: '#c6abd8',
+        cat: '#f4d06f',
+        bird: '#4fb3b0',
+        other: '#e9a89b',
+      },
+      borderRadius: {
+        blob: '60% 40% 50% 50% / 60% 50% 50% 40%',
+      },
+    },
+  },
+  corePlugins: {
+    transform: true, // Enable transform utilities
+  },
+  plugins: [],
+}
